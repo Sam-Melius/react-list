9afbd8e65a553da9f300ddf369a332ec612c9194
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-01-27 12:18:45 -0700

    created instruments array

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import { genres } from './Genres';
 import GenresList from './GenresList';
 
+
 function App() {
   return (
     <div className="App">
         Render all your lists here. Pass the arrays as props.
       <GenresList genres={genres} />
+      <InstrumentsList instruments={instruments} />
     </div>
   );
 }
---
+++ src/instruments.js
@@ -0,0 +1,22 @@
+export const instruments = [
+  {
+    name: 'Electric Guitar',
+    age: 1968,
+    image_url: 'https://i5.walmartimages.com/asr/2726b417-d273-4b6b-a649-08f8afe75c3f_1.b0b4dc54dba61c684540bd4f0efaaed5.jpeg?odnHeight=2000&odnWidth=2000&odnBg=ffffff',
+    color: 'white'
+  },
+
+  {
+    name: 'Bass Guitar',
+    age: 1990,
+    image_url: 'https://img.audiofanzine.com/images/u/product/normal/fender-standard-precision-bass-1990-2005-245794.jpg',
+    color: 'black'
+  },
+
+  {
+    name: 'Drums',
+    age: 1950,
+    image_url: 'https://drummagazine.com/wp-content/uploads/2016/08/Gretsch-Progressive-Jazz-Kits-Origin-Of-Cool.jpg',
+    color: 'custom'
+  }
+];
