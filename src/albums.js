4a73a4b9f05ecf5fbab60f948dc676f3e2289ce5
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-01-27 13:28:36 -0700

    created albums array

---
+++ src/albums.js
@@ -0,0 +1,31 @@
+export const albums = [
+  {
+    title: 'Subliminal Verses',
+    artist: 'Slipknot',
+    tracks: [
+      'Duality',
+      'Vermilion',
+      'Before I Forget' 
+    ]
+  },
+
+  {
+    title: 'Led Zeppelin IV',
+    artist: 'Led Zeppelin',
+    tracks: [
+      'Black Dog',
+      'Rock and Roll',
+      'Stairway to Heaven' 
+    ]
+  },
+
+  {
+    title: 'London Calling',
+    artist: 'The Clash',
+    tracks: [
+      'London Calling',
+      'Lost in the Supermarket',
+      'Revolution Rock'
+    ]
+  },
+];
