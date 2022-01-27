c2808dc507518a9accc9599af2b2089ec4bab1f7
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-01-27 13:06:37 -0700

    created musicians array

---
+++ src/musicians.js
@@ -0,0 +1,35 @@
+export const musicians = [
+  {
+    name: 'Corey',
+    url_image: 'https://www.metalsucks.net/wp-content/uploads/2017/05/corey-taylor-masks-thru-the-ages-1280x720.jpg',
+    position: 'Singer',
+    band: {
+      name: 'Slipknot',
+      type: 'Metal',
+      img: 'https://64.media.tumblr.com/a52c315acee3dabb0bf4f6953e06c3d4/tumblr_nmkcpaURGC1rrk5zio1_640.jpg'
+    }
+  },
+
+  {
+    name: 'John',
+    url_image: 'https://www.telegraph.co.uk/content/dam/news/2017/09/25/GettyImages-77186814_trans_NvBQzQNjv4BqCjWi45DX1ptcMBvOy6PVuURq-7ZVVvehvytxIsJeJIc.jpg',
+    position: 'Drummer',
+    band: {
+      name: 'Led Zeppelin',
+      type: 'Rock',
+      img: 'https://lite-images-i.scdn.co/image/ab67616d0000b27336adde683a3b172bf8eb6ab1'
+    }
+  },
+
+  {
+    name: 'Mick',
+    url_image: 'https://i.pinimg.com/originals/4d/97/eb/4d97eb116498adc572188016de819457.png',
+    position: 'Guitarist',
+    band: {
+      name: 'The Clash',
+      type: 'Punk',
+      img: 'https://www.rockarchive.com/media/4199/the-clash-cl002moho.png?crop=0.088908646365859059,0.12575647587442365,0.077572793954212238,0.30833942586572627&cropmode=percentage&width=800&height=800&rnd=132766913940000000&overlay=watermark.png&overlay.size=230,20&overlay.position=0,780'
+    }
+  }
+    
+];
